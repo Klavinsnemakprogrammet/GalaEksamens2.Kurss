0fa7922a62aece50273e6174c656db992d72a857
Klavinsnemakprogrammet <auznieki@gmail.com>
2025-06-08 17:10:34 +0300

    pielikta restart poga un izlabots mazs bug ar bildi

--- PythonFunkcijuTests/FunkcijuTests.py
+++ PythonFunkcijuTests/FunkcijuTests.py
@@ -9,7 +9,7 @@ jautajumi = [
     "KĀ VAR IZSAUKT ŠO FUNKCIJU?\n \n def function():\n print(""'Hello World!'"")",
     "KO IZVADĪS PROGRAMMA? \n\n def reizreikins(x): \n return x * 2\n print(reizreikins(5))",
     "KO IZDOS ŠĪ PROGRAMMA? \n\n def funkcija(x):""\n print(x)""\n my_function(x = 1)",
-    "KAS IR JĀIEVADA LAI FUNKCIJAS REZULTĀTS BŪTU 6?""\n\n def reizreikins(a, b):\nreturn a * b",
+    "KAS IR JĀIEVADA LAI FUNKCIJAS REZULTĀTS BŪTU 6?""\n\n def reizrekins(a, b):\nreturn a * b",
     "KAS TIKS IZDOTS PĒC ŠI KODA IZPILDES? \n\n def majas(valsts = ""'Norvēģja'""):\n  print(""Manas mājas ir "" + valsts)\n majas(""'Latvija'"")",
     "KĀ VAR IZSAUKT ŠO FUNKVIJU \n \n def HelloWorld():\n print(""'Hello World!'"")",
     "KO DARA COMMANDA 'PASS' ŠAJĀ KODĀ? \n\n def mana_funkcija():\n pass",
@@ -22,7 +22,7 @@ atbildes1 = [
     "print(funkcija())",
     "10",
     "x",
-    "reizreikins(2, 2)",
+    "reizrekins(2, 2)",
     "Error",
     "call HelloWorld()",
     "padod vērtības nākamai funkcijai",
@@ -35,7 +35,7 @@ atbildes2 = [
     "print(""Hello World!"")",
     "5",
     "1",
-    "reizreikins(3, 2)",
+    "reizrekins(3, 2)",
     "Ķengarags",
     "Hello World()",
     "veic loop",
@@ -74,7 +74,7 @@ atbildes = [
     "funkcija()",
     "10",
     "1",
-    "reizreikins(3, 2)",
+    "reizrekins(3, 2)",
     "Latvija",
     "HelloWorld()",
     "Neko",
@@ -163,17 +163,32 @@ def funkJAUTAJUMS():
         elif punkti>5:
             txt = f"Ir vel ko mācīties \n({punkti} punkti)"       
         elif punkti>1:
-            txt = f"Nu līdz 2niekam daudz nevaig!\nDiemžēl liela daļa tavu atbilžu bija nepareizas. ({punkti} punkti)"
+            txt = f"Nu līdz 2niekam daudz nevajag!\nDiemžēl liela daļa tavu atbilžu bija nepareizas. ({punkti} punkti)"
         elif punkti>0:
-            txt = f"Nu līdz 2niekam daudz nevaig!\nKā var pat 1 punktu neiegūt? ({punkti} punkts)"
+            txt = f"Nu līdz 2niekam daudz nevajag!\nKā var pat 1 punktu neiegūt? ({punkti} punkts)"
         else:
-            txt = f"Nu līdz 2niekam daudz nevaig!\nDiemžēl tavas atbildes bija nepareizas. ({punkti} punkti)"
+            txt = f"Nu līdz 2niekam daudz nevajag!\nDiemžēl tavas atbildes bija nepareizas. ({punkti} punkti)"
         rezult.config(pady = 20, text=txt, font=('Verdana', 14), fg="#FFFFFF", bg="#3F3F3F")
+
+def restart():
+    global piemers, punkti
+    piemers = 0
+    punkti = 0
+    rezult.config(text="rezultats", font=('Verdana', 14), fg="white", bg="#3F3F3F")
+    rezultata_skats.pack_forget()
+    jautNr.config(text=f'{piemers+1}. no 10 jautājumiem')
+    rezultats.config(text='0 punkti')
+    jautajums.config(text=jautajumi[seciba[piemers]])
+    atbilde1.config(text=atbildes1[seciba[piemers]])
+    atbilde2.config(text=atbildes2[seciba[piemers]])
+    atbilde3.config(text=atbildes3[seciba[piemers]])
+    atbilde4.config(text=atbildes4[seciba[piemers]])
+    jautajumu_skats.pack(fill='both', expand=True)
 
 #--------------------------------- programmas logs ------------------------------#
 root = tk.Tk()
 root.title("Python Funkciju Tests")
-root.geometry("1000x700")  
+root.geometry("1000x1000")  
 root.resizable(True, True) #var mainit logu izmērus
 root.configure(bg="#3F3F3F")
 
@@ -184,13 +199,14 @@ sakuma_skats = tk.Frame(root, bg="#3F3F3F")
 uzraksts_sakums = tk.Label(sakuma_skats, text="Python Funkciju Tests", font=('Verdana', 24, 'bold'), fg="#005994", bg="#3F3F3F")
 uzraksts_sakums.pack(pady=20)
 # ielādē attēlu
-image = tk.PhotoImage(file="resources/pythonlogo.png")  
+image = tk.PhotoImage(file="pythonlogo.png")  
 # izveido Label, lai parādītu attēlu
 attels_sakums = tk.Label(sakuma_skats, image=image, bg="#3F3F3F")
 attels_sakums.pack(pady=10)
 # izveido pogu
 poga = tk.Button(sakuma_skats, text="Sākt", command=funkSAKT, width=20, height=3, bg = 'lightgrey', fg = '#005994', font = ('Verdana', 20, 'bold'))
-poga.pack(pady=10)
+
+poga.pack(pady=150)
 
 #------------------------------- jautājumu skats -------------------------------#
 jautajumu_skats = tk.Frame(root, bg="#3F3F3F")
@@ -199,22 +215,10 @@ jautajumu_skats = tk.Frame(root, bg="#3F3F3F")
 jautajumu_skats.grid_columnconfigure(0, weight=1)
 jautajumu_skats.grid_columnconfigure(1, weight=1)
 # izvieto 2 elementus (jautājuma numurs un punkti) vienu otram blakus, katru savā kolonnā
-jautNr = tk.Label(
-    jautajumu_skats,
-    text=f'{piemers+1}. no 10 jautājumiem',
-    font=('Verdana', 12, 'bold'),
-    bg="#3F3F3F",
-    fg="white"  
-)
+jautNr = tk.Label(jautajumu_skats,text=f'{piemers+1}. no 10 jautājumiem',font=('Verdana', 12, 'bold'),bg="#3F3F3F",fg="white"  )
 jautNr.grid(row=0, column=0, padx=25, pady=25, sticky="w")
 
-rezultats = tk.Label(
-    jautajumu_skats,
-    text='0 punkti',
-    font=('Verdana', 12, 'bold'),
-    bg="#3F3F3F",
-    fg="white"  
-)
+rezultats = tk.Label(jautajumu_skats,text='0 punkti',font=('Verdana', 12, 'bold'),bg="#3F3F3F",fg="white"  )
 rezultats.grid(row=0, column=1, padx=25, pady=25, sticky="e")
 jautajums = tk.Label(jautajumu_skats, text = jautajumi[seciba[piemers]], font = ('Verdana', 15, 'bold'), fg = "#005994", bg="#3F3F3F")
 jautajums.grid(row=1, column=0, columnspan=2, pady=70)
@@ -239,6 +243,10 @@ attels_rezultats.pack(pady=10)
 # izveido rezultāta logu
 rezult = tk.Label(rezultata_skats, text="rezultats", font=('Verdana', 14), bg="#3F3F3F")
 rezult.pack(pady=20)
+
+restart_btn = tk.Button(rezultata_skats,text="Sākt no jauna", command=restart,width=20,height=2,bg='lightgrey',fg='#005994',font=('Verdana', 14, 'bold'))
+restart_btn.pack(pady=10)
+
 
 # Sākuma skata rādīšana
 sakuma_skats.pack(fill='both', expand=True)
